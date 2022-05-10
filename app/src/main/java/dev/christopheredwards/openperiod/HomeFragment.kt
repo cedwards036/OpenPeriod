8cd9c2a1aa54ffffd73e102e0e05e9779ee548ab
Christopher Edwards <cedwards036@gmail.com>
2022-05-09 21:16:12 -0400

    set up basic navigation and placeholder home page fragment

---
+++ app/src/main/java/dev/christopheredwards/openperiod/HomeFragment.kt
@@ -0,0 +1,22 @@
+package dev.christopheredwards.openperiod
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import dev.christopheredwards.openperiod.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding: FragmentHomeBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_home, container, false
+        )
+        return binding.root
+    }
+}
